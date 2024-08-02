17f37853b2c9ac41d09b17c2803b9fd52e698bad
Coshiloco <pablohurtadohg@gmail.com>
2024-08-02 19:16:30 +0200

    For backend

--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -1,14 +1,22 @@
 // src/services/mockApi.js
+
+import axios from "axios";
+import { API_URL } from "../config/configfrontend";
+
 const mockPins = [
     { id: 1, lat: 40.7128, lng: -74.0060, name: "New York" },
     { id: 2, lat: 51.5074, lng: -0.1278, name: "London" },
     { id: 3, lat: 48.8566, lng: 2.3522, name: "Paris" },
   ];
   
-  export const getPins = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(mockPins), 500);
-    });
+  export const getPins = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/pins`);
+      return response.data;
+    } catch (error) {
+      console.error('Error al obtener los pines:', error);
+      throw error;
+    }
   };
   
   export const addPin = (newPin) => {
