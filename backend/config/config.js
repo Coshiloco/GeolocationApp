950d244f83b8227a03a48129ae440ef97a97885e
Coshiloco <pablohurtadohg@gmail.com>
2024-08-02 22:56:46 +0200

    url for connect MongoDB

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,1 +1,1 @@
-export const MONGO_URL = 'mongodb+srv://admin:admin@cluster0.p1tn4qi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+export const MONGO_URL = 'mongodb+srv://usuariodetumongo:passworddetumongo@cluster0.p1tn4qi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
